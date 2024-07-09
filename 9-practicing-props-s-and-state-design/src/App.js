b21c31118a13c112d6ab54b7683a2d7923b73e23
Md Mostafizur Rahman <mostafizur1102@gmail.com>
2024-07-09 13:31:09 +0600

    12-20

---
+++ 9-practicing-props-s-and-state-design/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Dropdown from "./components/Dropdown";
+
+function App() {
+  const [selection, setSelection] = useState(null)
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
+  ]
+
+  return (
+    <div className="App m-4">
+      <center>
+        <Dropdown value={selection} onChange={handleSelect} options={options} />
+      </center>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+undefined || 100
+=> 100
+
+
+*/
