815eff6af3cdaacfc6aca0323c05aeb0e9e29cc7
Md Mostafizur Rahman <mostafizur1102@gmail.com>
2024-07-16 20:42:13 +0600

    reuseable navigation starts

---
+++ 10-reuseable-navigation/src/App.js
@@ -0,0 +1,20 @@
+
+function App() {
+
+  return (
+    <div className="App m-4">
+      <center className='flex'>
+
+      </center>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+undefined || 100
+=> 100
+
+
+*/
---
+++ 10-reuseable-navigation/src/pages/AccordioPage.js
@@ -0,0 +1,31 @@
+import Accordion from "../components/Accordion";
+
+function Accordion() {
+  const items = [
+  {
+    id: 1,
+    label: "Can i use react?",
+    content: "Yes you can. Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can."
+  },
+  {
+    id:2,
+    label: "Can i use js?",
+    content: "Yes you can. Yes you can. Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can."
+  },
+  {
+    id:3,
+    label: "Can i use django?",
+    content: "Yes you can. Yes you can. Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can.Yes you can."
+  }
+  ];
+
+  return (
+    <div className="App py-3">
+      <center>
+      </center>
+      <Accordion items={items} />
+    </div>
+  );
+}
+
+export default Accordion;
