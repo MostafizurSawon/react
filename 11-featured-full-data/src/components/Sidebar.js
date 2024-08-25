5dde4ff2a7825e68e0bea25b1a11f7ec1c2a4b2d
Md Mostafizur Rahman <mostafizur1102@gmail.com>
2024-08-25 14:50:41 +0600

    16->7

--- 11-featured-full-data/src/components/Sidebar.js
+++ 11-featured-full-data/src/components/Sidebar.js
@@ -11,7 +11,7 @@ function Sidebar(){
 
   const renderedLinks = links.map((link) => {
     return <Link key={link.label} to={link.path}
-     className="mb-4 bg-blue-300"
+     className="mb-4  "
      activeClassName="font-bold border-l-4 border-blue-500 pl-2"
      >{link.label}</Link>;
   })
--- 11-featured-full-data/src/components/Table.js
+++ 11-featured-full-data/src/components/Table.js
@@ -1,8 +1,34 @@
 
 
-function Table() {
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>
+  });
+
+  const renderedRows = data.map((rowData)=>{
+    const renderedCells = config.map((column) => {
+      return <td className="p-2" key={column.label}>{column.render(rowData)}</td>
+    });
+
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
   return (
-    <div>Table</div>
+    <table className="table-auto border-spacing-2">
+    {/* Table Data Found: {data.length} */}
+      <thead>
+        <tr className="border-b-2">
+          {renderedHeaders}
+        </tr>
+      </thead>
+      <tbody>
+        {renderedRows}
+      </tbody>
+    </table>
   )
 }
 
